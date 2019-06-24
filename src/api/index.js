639aea966db3884c9263f051dc85e639e7a19a75
adeewu <adeewu@outlook.com>
2019-06-24 17:12:14 +0800

    调整适应netcore后台并使用关键字做一个例子

--- src/api/index.js
+++ src/api/index.js
@@ -1,9 +1,78 @@
 import request from '../utils/request';
+import { Promise } from 'q';
 
-export const fetchData = (query) => {
-    return request({
-        url: '/ms/table/list',
-        method: 'post',
-        data: query
-    })
+// import Vue from 'vue'
+// import Dialog from '../components/page/DragDialog.vue'
+
+import { Message } from 'element-ui';
+
+function toReqeust(config) {
+    var requestPromise = new Promise((resolve, reject) => {
+        var result = request(config);
+
+        result.catch(reason => {
+            console.log(reason);
+
+            // var dialogComponent = Vue.extend(Dialog);
+            // var dialog = new dialogComponent({
+            //     data: {
+            //         visible: true,
+            //         message: reason.Message
+            //     }
+            // }).$mount();
+            // document.querySelector('body').appendChild(dialog.$el);
+            Message({
+                message: reason.Message,
+                type: 'error',
+                duration: 3000,
+                showClose: true
+            })
+
+            reject(reason);
+        });
+
+        result.then(data => {
+            resolve(data.Data);
+        })
+    });
+
+    return requestPromise;
+}
+
+export const fetchData = {
+    account: {
+        login: (username, password) => {
+            return toReqeust({
+                url: `/api/Account/Login`,
+                params: {
+                    username: username,
+                    password: password
+                },
+                method: 'post'
+            });
+        }
+    },
+    filter: {
+        list: params => {
+            return toReqeust({
+                url: `/api/Keyword`,
+                params: params,
+                method: 'get'
+            });
+        },
+        save: data => {
+            return toReqeust({
+                url: `/api/Keyword`,
+                data: data,
+                method: 'post'
+            })
+        },
+        delete: id => {
+            return toReqeust({
+                url: `/api/Keyword`,
+                params: { id: id },
+                method: 'delete'
+            })
+        }
+    }
 }
--- src/utils/request.js
+++ src/utils/request.js
@@ -2,26 +2,62 @@ import axios from 'axios';
 
 const service = axios.create({
     // process.env.NODE_ENV === 'development' 来判断是否开发环境
-    baseURL: 'https://www.easy-mock.com/mock/592501a391470c0ac1fab128',
+    baseURL: 'https://egg-go-api-dev.huobsj.com',
     timeout: 5000
 })
 
-service.interceptors.request.use( config => {
+service.interceptors.request.use(config => {
+    var token = localStorage.getItem('Token');
+    if (token) {
+        config.headers.common.Token = token;
+    }
+
     return config;
 }, error => {
-    console.log(error);
-    return Promise.reject();
+    console.log('service.interceptors.request error', error);
+    return Promise.reject({ Code: response.status, Message: '连接服务器失败' });
 })
 
 service.interceptors.response.use(response => {
-    if(response.status === 200){
-        return response.data;
-    }else{
-        Promise.reject();
+    if (response.status !== 200) {
+        return Promise.reject({ Code: response.status, Message: '服务器发生失败' });
+    }
+
+    if (!response.data.Code) {
+        return Promise.resolve({ Code: 200, Data: response.data });
+    }
+
+    if (response.data.Code === 200) {
+        return Promise.resolve(response.data);
+    }
+
+    if (response.data.Code === 204) {
+        return { Code: 200 };
+    }
+
+    if (response.data.Code === 401) {
+        console.log('service.interceptors.response', '请求未授权');
+        return Promise.reject({ Code: 401, Message: '请求未授权' });
+    }
+
+    if (response.data.Message) {
+        console.log('service.interceptors.response', '请求已完成，但服务器处理失败');
+        console.log('service.interceptors.response', response.data);
+        return Promise.reject({ Code: response.data.Code, Message: '请求已完成，但服务器处理失败', Data: response.data.Data });
     }
 }, error => {
-    console.log(error);
-    return Promise.reject();
+    console.log('service.interceptors.response error', error);
+
+    if (!error.response) {
+        return Promise.reject({ Code: 0, Message: '连接服务器失败' });
+    }
+
+    if (!error.response.data) {
+        return Promise.reject({ Code: 0, Message: '请求已完成，但服务器处理失败' });
+    }
+    else {
+        return Promise.reject(error.response.data);
+    }
 })
 
 export default service;
